27ceba7a780de5ac2f60f08ff460dd30a94cc9c3
amarjeet-kaloty <amarjeet.kaloty@yahoo.com>
2025-04-10 20:42:02 -0400

    Initial commit: setup

---
+++ src/main/java/com/SpringReactCollective/The/Online/Outfitters/TheOnlineOutfittersApplication.java
@@ -0,0 +1,13 @@
+package com.SpringReactCollective.The.Online.Outfitters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheOnlineOutfittersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheOnlineOutfittersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringReactCollective/The/Online/Outfitters/TheOnlineOutfittersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringReactCollective.The.Online.Outfitters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheOnlineOutfittersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
